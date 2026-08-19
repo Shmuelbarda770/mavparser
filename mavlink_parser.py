f00cd1d0ab075291d05f0ab874791d8a39c71eeb
shmuel <shmuelbarda770@gmail.com>
2026-08-19 19:30:15 +0300

    good

--- mavlink_parser.py
+++ mavlink_parser.py
@@ -4,22 +4,20 @@ from datetime import datetime
 try:
     start_time = datetime.now()  
 
-    mav = mavutil.mavlink_connection('log_file_test_01.bin')
+    mav = mavutil.mavlink_connection('data/log_file_test_01.bin')
     count = 0
     errors = 0
 
     while True:
-        msg = mav.recv_match()
+        msg = mav.recv_match().to_dict()
+        print(msg)
         if msg is None:
             break
-        count += 1
 
 except Exception as e:
-    errors += 1
     print(f'Part : Error - {str(e)[:100]}')
 
 end_time = datetime.now() 
 duration = end_time - start_time
 
-print(f'Part : Parsed {count} messages with {errors} errors.')
 print(f'Time taken: {duration}')
