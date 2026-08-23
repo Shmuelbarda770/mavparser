53fbe33b8f440cbea41c79121f6e15ee2acbace0
shmuel <shmuelbarda770@gmail.com>
2026-08-23 14:56:16 +0300

    test if we suurts all hw

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 """Build configuration for the CPython extension module."""
 
+import sys
 from setuptools import Extension, setup
 
+if sys.platform == "win32":
+    extra_compile_args = ["/O2", "/W3"]
+else:
+    extra_compile_args = ["-O3", "-Wall", "-Wextra"]
 
 setup(
     ext_modules=[
         Extension(
             "mavparser._mavparser",
             sources=["mavparser/parser.c"],
-            extra_compile_args=["-O3", "-Wall", "-Wextra"],
+            extra_compile_args=extra_compile_args,
         )
     ]
 )
